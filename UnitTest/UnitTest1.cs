de2cdc3094a204f653a893f91cbdfc52aa55ec99
Kote\Kote_Animen <sverh.il@yandex.ru>
2021-04-06 13:49:40 +0300

    юнит тесты написаны

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -36,5 +36,59 @@ namespace UnitTest
             int[] numbers = new int[] { 1, 2, 3 };
             Assert.AreEqual(0, task.SummThreeNumbers(numbers));
         }
+
+        [TestMethod]
+        public void RejectionMaxMass1()
+        {
+            TaskMethods task = new TaskMethods();
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+            int[] expected = new int[] { 4, 3, 2, 1, 0 };
+            int max;
+            int[] reject;
+            task.RejectionMaxMass(numbers, out max, out reject);
+            Assert.AreEqual(5, max);
+            for(int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], reject[i]);
+            }
+            
+        }
+
+        [TestMethod]
+        public void RejectionMaxMass2()
+        {
+            TaskMethods task = new TaskMethods();
+            int[] numbers = new int[] { 5, 4, 3, 2, 1 };
+            int[] expected = new int[] { 0, 1, 2, 3, 4 };
+            int max;
+            int[] reject;
+            task.RejectionMaxMass(numbers, out max, out reject);
+            Assert.AreEqual(5, max);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], reject[i]);
+            }
+        }
+
+        [TestMethod]
+        public void ExchangeMaxMinRows1()
+        {
+            TaskMethods task = new TaskMethods();
+
+            double[,] table = new double[,] { {0, 1, 2, 3 }, { -1, 3, 6, 3 }, {10, 7, 5, 6 } };
+            double[,] changedTable;
+            double[,] expected = new double[,] { { 10, 7, 5, 6 }, { -1, 3, 6, 3 }, { 0, 1, 2, 3 } };
+            
+            task.ExchangeMaxMinRows(table, out changedTable);
+            for(int i = 0; i < table.GetLength(0); i++)
+            {
+                for(int j = 0; j < table.GetLength(1); j++)
+                {
+                    Assert.AreEqual(expected[i, j], changedTable[i, j]);
+                }
+            }
+            
+        }
+
     }
 }
